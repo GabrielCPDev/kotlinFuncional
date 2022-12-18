45803bda323cbf5fdba31328174ab207ca3fd1b7
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-12-18 15:14:50 -0300

    wip

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,16 +5,25 @@ fun main() {
     expressaoLambda()
     funcaoAnonima()
 
-    val calculaBonificacao: (salario: Double) -> Double = lambda@ {
-        return@lambda if (it < 1000 ) it + 100.0 else it + 50
+    funcaoLambdaComMultiploRetorno()
+
+    funcaoAnonimaComMultiploRetorno()
+}
+
+fun funcaoLambdaComMultiploRetorno() {
+    val calculaBonificacao: (salario: Double) -> Double = lambda@{
+        return@lambda if (it < 1000) it + 100.0 else it + 50
     }
     println(calculaBonificacao(1900.0))
+}
 
-    val calculaBonificacaoAnonima: (salario: Double) -> Double =  fun (salario: Double):Double {
-        return if (salario < 1000 ) salario + 100.0 else salario + 50
+fun funcaoAnonimaComMultiploRetorno() {
+    val calculaBonificacaoAnonima: (salario: Double) -> Double = fun(salario: Double): Double {
+        return if (salario < 1000) salario + 100.0 else salario + 50
     }
     println(calculaBonificacaoAnonima(900.0))
 }
+
 fun expressaoLambda() {
     val minhaFuncaoLambda: (Int, Int) -> Int = { a , b ->
         println("minha funcao lambda")
