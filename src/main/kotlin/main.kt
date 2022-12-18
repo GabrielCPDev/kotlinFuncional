d52b44be37e27bfcbab3767b81ba6eb9b76cd6e5
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-12-18 13:54:13 -0300

    wip

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,33 +2,30 @@ fun main() {
     testaTipoFuncaoReferencia()
     testaTipoFuncaoClasse()
 
-    val minhaFuncaoLambda : () -> Unit = { println("minha funcao lambda") }
-
+    expressaoLambda()
+    funcaoAnonima()
+}
+fun expressaoLambda() {
+    val minhaFuncaoLambda: () -> Unit = { println("minha funcao lambda") }
     println(minhaFuncaoLambda())
-
-    val minhaFuncaoAnonima: () -> Unit = fun () {  println("minha funcao anonima") }
+}
+fun funcaoAnonima() {
+    val minhaFuncaoAnonima: () -> Unit = fun() { println("minha funcao anonima") }
     println(minhaFuncaoAnonima())
 }
-
 fun testaTipoFuncaoClasse() {
     val minhaFuncaoClasse = TesteClasse()
     println(minhaFuncaoClasse())
 }
-
 fun testaTipoFuncaoReferencia() {
     val minhaFuncao: (a: Int, b: Int) -> Unit = ::teste
-
     println(minhaFuncao(2, 3))
 }
-
 fun teste(a:Int?= 0, b: Int? = 0){
     println("executa teste! ${a!! + b!!}")
 }
-
 class TesteClasse() : () -> Unit{
     override fun invoke() {
         println("executa invoke de classe")
     }
-
-
 }
